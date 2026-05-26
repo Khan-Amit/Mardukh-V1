7b6f8359fa7415ffff178436fa3b004698f2514a
ENKI <amit.khanna.1082@gmail.com>
2026-05-26 13:16:50 +0700

    Create state_controller.c

---
+++ src/state_controller.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void write_telemetry_payload() {
+    // Logic layout routine to safely export telemetry state arrays to root path
+    printf("[STATE CONTROL] Syncing dynamic payload configurations with local canvas array.\n");
+}
