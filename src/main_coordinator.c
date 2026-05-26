fbd22e39add280eda62f25e5fe74b5853aca5190
ENKI <amit.khanna.1082@gmail.com>
2026-05-26 13:08:14 +0700

    Create main_coordinator.c

---
+++ src/main_coordinator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("[MARDUKH COORDINATOR] Initializing core coordination loop engine...\n");
+    printf("[MARDUKH COORDINATOR] Mapping active dynamic arrays securely.\n");
+    return 0;
+}
